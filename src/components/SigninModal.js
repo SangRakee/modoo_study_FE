041cb5c777ec401cdf032a78b054e404b74b46bd
SangRakKun <leesangrak0307@gmail.com>
2021-07-16 10:18:33 +0900

    feature : store JWT in localstorage & set Email on MyPage

--- src/components/SigninModal.js
+++ src/components/SigninModal.js
@@ -6,9 +6,10 @@ import "../assets/css/modal.css"
 
 function SigninModal(props) {
     // 열기, 닫기, 모달 헤더 텍스트를 부모로부터 받아옴
-    const { open, close, header } = props;
+    const { isOpen, close, isjoinlogin ,header } = props;
     let [JoinLoign,setJoinLogin] = useState('로그인')
     const history = useHistory()
+    // console.log(isjoinlogin)
 
     let [username, setUsername] = useState()
     let [userpassword, setUserPassword] = useState()
@@ -29,7 +30,6 @@ function SigninModal(props) {
     }
     data = data.join("&");
 
-
     const handleNameChange = (e) => {
         setUsername(e.target.value)
     }
@@ -40,16 +40,16 @@ function SigninModal(props) {
 
     return (
         // 모달이 열릴때 openModal 클래스가 생성된다.
-        <div className={ open ? 'openModal modal' : 'modal' }>
-            { open ? (
+        <div className={ isOpen ? 'openModal modal' : 'modal' }>
+            { isOpen ? (
                 <section>
                     <header>
-                        {JoinLoign}
+                        {isjoinlogin}
                         <button className="close" onClick={close}> &times; </button>
                     </header>
                     <main>
                         {
-                            JoinLoign === '로그인'
+                            isjoinlogin === '로그인'
                                 ? (
                                 <>
                                     <div className="loginForm">
@@ -110,37 +110,38 @@ function SigninModal(props) {
                                                         alert('아이디 또는 비밀번호를 확인해주세요.');
                                                     }
                                                 });
-                                        }}>{JoinLoign}</button>
+                                        }}>{isjoinlogin}</button>
                                     </div>
                                 </>
                                 )
                                 :(
                                     <>
                                         <div className="loginForm">
-                                            <input
-                                                name="email"
-                                                className="loginId"
-                                                type="email"
-                                                placeholder="아이디"
-                                                onChange={handleNameChange}
-                                            />
-                                            <input
-                                                name="password"
-                                                className="loginPw"
-                                                type="password"
-                                                placeholder="비밀번호"
-                                                onChange={handlePasswordChange}
-                                            />
-                                            {/*{props.children} app값 불러오기*/ }
-                                            <div className="loginOptions">
-                                                <div className="loginSession">
-                                                    <input type="checkbox" id="loginSession"></input>
-                                                    <label for="loginSession">로그인 유지</label>
-                                                </div>
-                                                <div className="findMyAccount">
-                                                    <a href="/"><span>아이디/비밀번호 찾기</span></a>
-                                                </div>
+                                            <div className="nicknameForm">
+                                                <input
+                                                    name="nickname"
+                                                    className="loginId"
+                                                    type="nickname"
+                                                    placeholder="닉네임"
+                                                    // onChange={this.loginHandler}
+                                                />
+                                                <button>중복확인</button>
                                             </div>
+
+                                            <input
+                                                name="location"
+                                                className="location"
+                                                type="location"
+                                                placeholder="선호지역"
+                                                // onChange={this.loginHandler}
+                                            />
+                                            <div className="signupSession">
+                                                <input type="checkbox" id="category1"></input>
+                                                <label htmlFor="signupSession">NCS/인적성</label>
+                                                <input type="checkbox" id="category2"></input>
+                                                <label htmlFor="signupSession">면접</label>
+                                            </div>
+                                            {/*{props.children} app값 불러오기*/ }
                                         </div>
                                         <div className="loginSubmit">
                                             {/*<input class="loginSubmitBtn" type="submit" value="로그인하기"/>*/}
@@ -170,7 +171,7 @@ function SigninModal(props) {
                                                 //     });
                                                 // .catch(error => alert(error));
                                             }}
-                                            >{JoinLoign}</button>
+                                            >{isjoinlogin}</button>
                                         </div>
                                     </>
                                 )
@@ -178,14 +179,15 @@ function SigninModal(props) {
                     </main>
                     <footer>
                         {
-                            JoinLoign === '회원가입'
+                            isjoinlogin === '회원가입'
                                 ? (
                                     <>
                                         <div className="join">
                                             <button onClick={(e)=>{
                                                 e.preventDefault();
                                                 setJoinLogin('로그인');
-                                            }}>로그인</button>
+                                                props.signin();
+                                            }}>로그인화면 이동</button>
                                         </div>
                                     </>
                                 )
@@ -198,7 +200,8 @@ function SigninModal(props) {
                                             <button onClick={(e)=>{
                                                 e.preventDefault();
                                                 setJoinLogin('회원가입');
-                                            }}>회원가입</button>
+                                                props.signup();
+                                            }}>회원가입 화면 이동</button>
                                         </div>
                                     </>
                                 )
